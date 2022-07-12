4311479963299a9b904028fd9ae91ff4b972f26d
Olaf <kurasolaf@gmail.com>
2022-07-12 11:42:28 +0200

    Add project files.

---
+++ AlgorithmNumber4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmNumber4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+        }
+    }
+}
