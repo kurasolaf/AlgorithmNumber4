1195de9af9371596e02f0a720037bf5fd80baec6
Olaf <kurasolaf@gmail.com>
2022-07-12 13:02:52 +0200

    final version

--- AlgorithmNumber4/Program.cs
+++ AlgorithmNumber4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace AlgorithmNumber4
 {
@@ -6,13 +8,26 @@ namespace AlgorithmNumber4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
+            Console.WriteLine("Please enter number");
 
+            var input = Console.ReadLine(); 
+            char dotCheck = '.';
+            bool digitCheck = input.Contains(dotCheck);
 
-
+            if (digitCheck == true) 
+            {
+                Console.WriteLine(input.Length -1);
+            }
+            else
+            {
+                Console.WriteLine(input.Length);
+            }
 
 
         }
+
     }
-}
+    
+ }
+
